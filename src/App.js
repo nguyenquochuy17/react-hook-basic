a7024077cfb4280ea4659506f808ae661a5450fa
nguyenquochuy <s3818764@rmit.edu.vn>
2021-06-30 10:58:28 +0700

    box color

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.scss';
-
+import ColorBox from './components/ColorBox/index'
 function App() {
+  console.log("Render App")
   return (
     <div className="App">
       <h1> Welcome Nguyen Quoc Huy</h1>
+      <ColorBox></ColorBox>
     </div>
   );
 }
---
+++ src/components/ColorBox/index.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+ColorBox.propTypes = {
+
+};
+function getRandomColor() {
+    const colorList = ["deeppink", "green", "yellow", "black", "blue"]
+    var randomIndex = Math.trunc(Math.random() * 5)
+    return colorList[randomIndex]
+}
+function ColorBox(props) {
+    const [color, setColor] = useState(() => {
+        const initColor = localStorage.getItem("box-color") || "deeppink"
+        console.log(initColor)
+        return initColor
+    });
+    console.log("Color Box")
+    function handleBoxClick() {
+        var newColor = getRandomColor()
+        setColor(newColor)
+        localStorage.setItem("box-color", newColor)
+    }
+    return (
+        <div className="color-box"
+            style={{ backgroundColor: color }}
+            onClick={handleBoxClick}>
+            Color Box
+        </div>
+    );
+}
+
+export default ColorBox;
